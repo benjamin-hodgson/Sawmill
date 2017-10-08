2593ca903c6b141f6136ea939da662d2b1f1e7b8
Benjamin Hodgson <bhodgson@stackoverflow.com>
2017-10-08 14:04:10 +0100

    Wrote a package for manipulating Newtonsoft.Json objects

---
+++ Sawmill.Newtonsoft.Json/JTokenRewriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace Sawmill.Newtonsoft.Json
+{
+    public class JTokenRewriter : IRewriter<JToken>
+    {
+        public Children<JToken> GetChildren(JToken value)
+            => value is JContainer c
+                ? Children.Many(c.Children())
+                : Children.None<JToken>();
+
+        public JToken SetChildren(Children<JToken> newChildren, JToken oldValue)
+        {
+            if (newChildren.NumberOfChildren == NumberOfChildren.None)
+            {
+                return oldValue;
+            }
+            var c = (JContainer)oldValue.DeepClone();
+            c.ReplaceAll(newChildren.Many);
+            return c;
+        }
+
+        public JToken RewriteChildren(Func<JToken, JToken> transformer, JToken oldValue)
+            => this.DefaultRewriteChildren(transformer, oldValue);
+    }
+}
