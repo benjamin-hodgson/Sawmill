5f474dd3db0d867b97dc909c1a1a7ca7838c672e
Benjamin Hodgson <bhodgson@stackoverflow.com>
2018-05-29 15:55:52 +0100

    revbump to 2.1.0

--- setversion.sh
+++ setversion.sh
@@ -4,4 +4,5 @@ sed -i '' "s/<Version>[0-9.]*<\/Version>/<Version>$1<\/Version>/g"\
     Sawmill.Newtonsoft.Json/Sawmill.Newtonsoft.Json.csproj\
     Sawmill.Microsoft.CodeAnalysis/Sawmill.Microsoft.CodeAnalysis.csproj\
     Sawmill.Microsoft.CodeAnalysis.CSharp/Sawmill.Microsoft.CodeAnalysis.CSharp.csproj\
-    Sawmill.Microsoft.CodeAnalysis.VisualBasic/Sawmill.Microsoft.CodeAnalysis.VisualBasic.csproj
+    Sawmill.Microsoft.CodeAnalysis.VisualBasic/Sawmill.Microsoft.CodeAnalysis.VisualBasic.csproj\
+    Sawmill.HtmlAgilityPack/Sawmill.HtmlAgilityPack.csproj\
