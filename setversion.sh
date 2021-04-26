4c73d574f6f258431b3e58c4eee94745d48cc17b
Benjamin Hodgson <bhodgson@stackoverflow.com>
2021-04-26 12:29:38 -0400

    Build stuff

--- setversion.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i '' "s/<Version>[0-9.]*<\/Version>/<Version>$1<\/Version>/g"\
-    Sawmill/Sawmill.csproj\
-    Sawmill.Newtonsoft.Json/Sawmill.Newtonsoft.Json.csproj\
-    Sawmill.Microsoft.CodeAnalysis/Sawmill.Microsoft.CodeAnalysis.csproj\
-    Sawmill.Microsoft.CodeAnalysis.CSharp/Sawmill.Microsoft.CodeAnalysis.CSharp.csproj\
-    Sawmill.Microsoft.CodeAnalysis.VisualBasic/Sawmill.Microsoft.CodeAnalysis.VisualBasic.csproj\
-    Sawmill.HtmlAgilityPack/Sawmill.HtmlAgilityPack.csproj\
